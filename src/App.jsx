e4d5ab0f0888568afb2f9029da7b9aced9c46013
chanathan03 <jkchan3@gmail.com>
2026-03-08 14:05:04 -0700

    fix hardcoded days to start from today

--- src/App.jsx
+++ src/App.jsx
@@ -24,11 +24,11 @@ const INITIAL_RESORT_DATA = {
       condition: "Deep Powder", lifts: "4/4", trails: "24/24"
     },
     forecast: [
-      { day: "Friday", high: 30, low: 22, snow: '12"' },
-      { day: "Saturday", high: 28, low: 18, snow: '6"' },
-      { day: "Sunday", high: 32, low: 24, snow: '2"' },
-      { day: "Monday", high: 34, low: 28, snow: '0"' },
-      { day: "Tuesday", high: 31, low: 22, snow: '8"' },
+      { day: "Thursday", high: 30, low: 22, snow: '12"' },
+      { day: "Wednesday", high: 28, low: 18, snow: '6"' },
+      { day: "Thursday", high: 32, low: 24, snow: '2"' },
+      { day: "Wednesday", high: 34, low: 28, snow: '0"' },
+      { day: "Thursday", high: 31, low: 22, snow: '8"' },
     ]
   },
   "Vail Mountain": {
@@ -38,11 +38,11 @@ const INITIAL_RESORT_DATA = {
       condition: "Powder", lifts: "27/33", trails: "221/277"
     },
     forecast: [
-      { day: "Friday", high: 27, low: 17, snow: '5"' },
-      { day: "Saturday", high: 22, low: 10, snow: '2"' },
-      { day: "Sunday", high: 43, low: 17, snow: '0"' },
-      { day: "Monday", high: 47, low: 24, snow: '0"' },
-      { day: "Tuesday", high: 44, low: 29, snow: '1"' },
+      { day: "Thursday", high: 27, low: 17, snow: '5"' },
+      { day: "Wednesday", high: 22, low: 10, snow: '2"' },
+      { day: "Thursday", high: 43, low: 17, snow: '0"' },
+      { day: "Wednesday", high: 47, low: 24, snow: '0"' },
+      { day: "Thursday", high: 44, low: 29, snow: '1"' },
     ]
   },
   "Mammoth Mountain": {
@@ -52,11 +52,11 @@ const INITIAL_RESORT_DATA = {
       condition: "Packed Powder", lifts: "21/25", trails: "145/175"
     },
     forecast: [
-      { day: "Friday", high: 28, low: 18, snow: '4"' },
-      { day: "Saturday", high: 25, low: 15, snow: '10"' },
-      { day: "Sunday", high: 22, low: 12, snow: '2"' },
-      { day: "Monday", high: 30, low: 20, snow: '0"' },
-      { day: "Tuesday", high: 35, low: 25, snow: '0"' },
+      { day: "Thursday", high: 28, low: 18, snow: '4"' },
+      { day: "Wednesday", high: 25, low: 15, snow: '10"' },
+      { day: "Thursday", high: 22, low: 12, snow: '2"' },
+      { day: "Wednesday", high: 30, low: 20, snow: '0"' },
+      { day: "Thursday", high: 35, low: 25, snow: '0"' },
     ]
   }
 };
